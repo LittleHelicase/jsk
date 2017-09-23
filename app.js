80b2283d2bbea5e0b4d2e9cf58af85f52670e3df
Volker Schmidt <volker.schmidt91@gmail.com>
2017-09-23 06:08:40 +0200

    JIT

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const _ = require('lodash')
 const base =
 {
 
-    //uncomment to enable reductions
+    // uncomment to enable reductions (VERY slow):
 
-    // I: [['S', 'K'], 'K'],
-    // U: [['S', 'I'], 'I'],
-    // Y: {f: [
-    //     {x: ['f', ['x', 'x']]},
-    //     {x: ['f', ['x', 'x']]}
-    // ]},
-    // T: 'K',
-    // F: ['K', 'I'],
-    // O: {f: {x: ['f', 'x']}},
-    // B: [['S', ['K', 'S']], 'K'],
-    // C: [['S', [['S', ['K', 'B']], 'S']], ['K', 'K']],
+    I: ['S', 'K', 'K'],
+    F: ['K', 'I'],
+    B: ['S', ['K', 'S'], 'K'],
+    C: ['S', ['S', ['K', 'B'], 'S'], ['K', 'K']],
 
+    Y: {f: [
+        {x: ['f', ['x', 'x']]},
+        {x: ['f', ['x', 'x']]}
+    ]},
+    U: [['S', 'I'], 'I'],
     K: 'T',
     not: 'C',
     succ: ['S', 'B'],
     zero: 'F',
-    one: 'O',
+    one: 'I',
+    two: ['S', 'B', 'one'],
+    three: ['S', 'B', 'two'],
     and: {p: {q: ['p', 'q', 'F']}},
     or: {p: {q: ['p', 'T', 'q']}},
     isZero: {n: ['n', ['K', 'F'], 'T']},
@@ -31,16 +31,11 @@ const base =
     geq: {m: {n: ['isZero', ['sub', 'n', 'm']]}},
     eq: {m: {n: ['and', ['leq', 'm', 'n'], ['geq', 'm', 'n']]}},
     pred: {n: {f: {x: ['n', {g: {h: ['h', ['g', 'f']]}}, ['K', 'x'], 'I']}}},
-    two: ['succ', 'one'],
-    three: ['succ', 'two'],
     add: {m: {n: {f: {x: ['m', 'f', ['n', 'f', 'x']]}}}},
     mul: 'B',
     sq: 'two',
     fac: ['Y', {f: {n: ['isZero', 'n', 'one', ['mul', 'n', ['f', ['pred', 'n']]]]}}],
-    div1: {c: {n: {m: {f: {x:
-        [{d: ['isZero', 'd', ['zero', 'f', 'x'], ['f', ['c', 'd', 'm', 'f', 'x']]]},
-        ['sub', 'n', 'm']]
-    }}}}},
+    div1: {c: {n: {m: {f: {x: ['isZero', ['sub', 'n', 'm'], 'x', ['f', ['c', ['sub', 'n', 'm'], 'm', 'f', 'x']]]}}}}},
     div: {n: ['Y', 'div1', ['succ', 'n']]},
 
     // church pair
@@ -50,11 +45,12 @@ const base =
 
     // cons list
     nil: 'F',
-    isNil: {l: ['l', {h: {t: {d: 'F'}}}, 'T']},
+    isNil: {l: ['l', {h: {t: ['K', 'F']}}, 'T']},
     cons: 'pair',
     head: 'first',
     tail: 'second',
-    at: {n: {l: ['head', ['n', 'tail', 'l']]}}
+    at: {n: {l: ['head', ['n', 'tail', 'l']]}},
+    isShorterThan: {n: {l: ['isNil', ['n', 'tail', 'l']]}}
 
 }
 
@@ -68,14 +64,19 @@ function numeral(n)
         case 3: return 'three'
     }
     let k = Math.floor(Math.sqrt(n))
-    let r = n - k * k
-    switch(r)
+    switch(n - k * k)
     {
         case 0: return ['sq', numeral(k)]
         case 1: return ['succ', ['sq', numeral(k)]]
         case 2: return ['succ', ['succ', ['sq', numeral(k)]]]
     }
-    return ['add', ['sq', numeral(k)], numeral(r)]
+    let h = Math.floor(n / 3)
+    switch(n - 3 * h)
+    {
+        case 0: return ['mul', 'three', numeral(h)]
+        case 1: return ['succ', ['mul', 'three', numeral(h)]]
+        case 2: return ['succ', ['succ', ['mul', 'three', numeral(h)]]]
+    }
 }
 
 function list(l)
@@ -103,7 +104,7 @@ function fn(arg, body)
 {
     let f = { }
     f[arg] = body
-    return f
+    return compile(f)
 }
 
 function size(code)
@@ -134,68 +135,88 @@ function fan(code)
 
 function compile(code)
 {
-    if(code in base)
-        return compile(base[code])
-    else if(Array.isArray(code))
-        return fan(_.map(code, compile))
-    else if(typeof code === 'object')
+    let a = arg(code), b = body(code)
+    if(a === b)
+        return 'I'
+    else if(!contains(b, a))
+        return ['K', b]
+    else if(Array.isArray(b))
     {
-        let a = arg(code), b = fan(body(code))
-        if(a === b)
-            return compile('I')
-        else if(!contains(b, a))
-            return compile(['K', b])
-        else if(Array.isArray(b) && !contains(b[0], a))
-            return compile(['B', b[0], fn(a, b[1])])
-        else if(Array.isArray(b) && !contains(b[1], a))
-            return compile(['C', fn(a, b[0]), b[1]])
-        else if(Array.isArray(b))
-            return compile(['S', fn(a, b[0]), fn(a, b[1])])
-        else if(typeof b === 'object')
-            return compile(fn(a, compile(fn(arg(b), body(b)))))
+        if(b.length > 2)
+            return fn(a, fan(b))
+        else if(!contains(b[0], a))
+            return ['B', b[0], fn(a, b[1])]
+        else if(!contains(b[1], a))
+            return ['C', fn(a, b[0]), b[1]]
+        else
+            return ['S', fn(a, b[0]), fn(a, b[1])]
     }
-    else return code
+    else if(typeof b === 'object')
+        return fn(a, compile(b))
+}
+
+const instructions =
+{
+    I: x => x,
+    T: (a, b) => a,
+    F: (a, b) => b,
+    S: (f, g, x) => [f, x, [g, x]],
+    B: (f, g, x) => [f, [g, x]],
+    C: (f, x, y) => [f, y, x],
 }
 
 function exec(code)
 {
-    if(!Array.isArray(code))
-        return code
     while(true)
     {
         while(Array.isArray(code[0]))
             code = _.concat(code[0], code.slice(1))
         switch(typeof code[0])
         {
-            case 'function':
-                let fn = code[0]
-                let args = code.splice(1, fn.length).map(exec)
-                code[0] = fn.apply(null, args)
-                continue
-        }
-        switch(code[0])
-        {
-            case 'I': code.shift(); continue
-            case 'U': code[0] = code[1]; continue
-            case 'Y': code[0] = code[1]; code[1] = ['Y', code[1]]; continue
-            case 'F': code.splice(0, 2); continue
-            case 'T': code.shift(); code.splice(1, 1); continue
-            case 'O': code.shift(); continue
-            case 'B': code.shift(); code.splice(1, 2, [code[1], code[2]]); continue
-            case 'C': code.shift(); let y = code[1]; code[1] = code[2]; code[2] = y; continue
-            case 'S': code.shift(); let z = code[2]; code[2] = [code[1], z]; code[1] = z; continue
-        }
-        switch(typeof code[0])
-        {
             case 'object':
-            case 'string':
                 code[0] = compile(code[0])
+                continue
+            case 'string':
+                if(code[0] in base)
+                {
+                    code[0] = base[code[0]]
+                    continue
+                }
+                let n = parseInt(code[0])
+                if(!isNaN(n))
+                {
+                    code[0] = numeral(n)
+                    continue
+                }
+            case 'function':
+                let f = code.shift(), args
+                if(f in instructions)
+                {
+                    f = instructions[f]
+                    args = code.splice(0, f.length)
+                }
+                else
+                {
+                    args = code.splice(0, f.length).map(exec)
+                }
+                let v = f.apply(null, args)
+                if(Array.isArray(v))
+                    code = _.concat(v, code)
+                else
+                    code.splice(0, 0, v)
                 continue
         }
         return code.length == 1 ? code[0] : code
     }
 }
 
-console.log(JSON.stringify(exec(['eq', ['fac', numeral(5)], numeral(120), true, false])))
-console.log(JSON.stringify(exec(['isNil', [numeral(9), 'tail', list(_.range(10))], true, false])))
-console.log(JSON.stringify(exec(['div', numeral(100), numeral(3), n => n+1, 0])))
+function run(...code)
+{
+    console.log('EXEC \t' + JSON.stringify(code))
+    console.log('===> \t' + JSON.stringify(exec(code)))
+    console.log()
+}
+
+run('at', '7', list(_.range(10)))
+run('eq', ['fac', '5'], '120', true, false)
+run('div', '234', '23', n => n+1, 0)
